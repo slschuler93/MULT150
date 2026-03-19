aa56c69dbb9dee611da58ad934e04dcb07250ff3
Sandra Lillian Schuler Contreras <slschuler93@gmail.com>
2026-03-19 03:39:33 -0700

    created TIY Hour 7 part 1 and Hour 7 scene

---
+++ sschuler_Hour7_P1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float health = 1004f;
+        float poisonDamage = 125.5f;
+        Debug.Log(poisonDamage);
+        Debug.Log(health);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log(health -= poisonDamage);
+        Debug.Log("Player has been unalived!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
