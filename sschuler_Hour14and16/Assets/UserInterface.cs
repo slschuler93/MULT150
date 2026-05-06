98bda34232744a19fb2633cf4c5fa784f2431994
Sandra Lillian Schuler Contreras <slschuler93@gmail.com>
2026-05-06 03:45:47 -0700

    created Hour 14

---
+++ sschuler_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,3 @@
+/* I added instructions to the UI layout, as well as a simple title for the game.*/
+/* I changed the background image and the coloring for it as well.*/
+/* I chose a different font and applied it to all text boxes in the scene.*/
