6476759e4b5de19f12b4885d9453d94debb21c20
Sandra Lillian Schuler Contreras <slschuler93@gmail.com>
2026-06-01 23:29:04 -0700

    created final project
    final part 1

---
+++ sschuler_Final/Modifications.cs
@@ -0,0 +1,60 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        CHAOS BALL
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: changed the color to yellow, intensity to 2
+
+        2 - fog
+        modifications: changed the color of the fog to pink and changed the density to .007
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: created a procedural skybox and made the sky tint purple and the ground pink. I changed the sun size to 0.5, the sun size convergence to 10, and the atmosphere thickness to 1.2. the exposure was changed to 2. 
+
+        CHAOS BALLS
+        4 - convert a chaos ball into a prefab, and replace all of them in the scene with instances of that prefab
+        [no record necessary]
+
+        5 - add particle system to the chaos ball prefab and customise at least 3 parameters
+        -All instances will update to include the particle system
+        coloured balls
+        parameter 1: 
+        parameter 2: 
+        parameter 3: 
+
+        6 - convert a coloured ball into a prefab, create variants for each of the colours, and replace them in the scene
+        -Don't forget to update the tags so that the game works
+        [no record necessary]
+
+        7 - apply at least 1 new texture while retaining the different colours
+        (add source url or package name)
+        texture source: 
+
+        GOALS
+        8 - modify light components (colour, range, intensity, etc)
+        modifications: blue intensity to 25 and darkened color, changed color of green to darker color, orange intensity changed to 25
+
+        WALL
+        9 - convert each to complex objects comprised of 3 or more shapes
+        -Try recreating them to be more interesting! castle walls, fences, pillars, etc
+        [no record necessary]
+
+        10 - use gameobject hierarchy to keep the wall prefab organised & apply at least 1 new texture
+        (add source url or package name)
+        texture source: https://assetstore.unity.com/packages/2d/textures-materials/brick/high-quality-bricks-walls-49581
+
+    */
+}
